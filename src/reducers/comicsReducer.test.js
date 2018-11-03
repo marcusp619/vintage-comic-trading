2743f9298a726eadfd4e01748ad06e472d9c6eaa
marcusp619 <marcusp619@gmail.com>
2018-11-03 12:51:38 -0600

    Pass comic reducer add comic test

--- src/reducers/comicsReducer.test.js
+++ src/reducers/comicsReducer.test.js
@@ -1,14 +1,40 @@
-import {comics, isLoading, hasErrored } from './comics-reducers'
+import {comics, isLoading, hasErrored} from './comics-reducers';
 
 describe('comics reducer', () => {
   it('should return the initial state', () => {
-    //setup 
-    const expected = []
+    //setup
+    const expected = [];
 
     //execution
-    const result = comics(undefined, [])
+    const result = comics(undefined, []);
 
     //expectation
-    expect(result).toEqual(expected)
-  })
-})
+    expect(result).toEqual(expected);
+  });
+
+  it('shoudl return state with new comic', () => {
+    //setup
+    const expected = [{
+      id: 1009718,
+      name: 'Wolverine',
+      description:
+        "Born with super-human senses and the power to heal from almost any wound, Wolverine was captured by a secret Canadian organization and given an unbreakable skeleton and claws. Treated like an animal, it took years for him to control himself. Now, he's a premiere member of both the X-Men and the Avengers.",
+    }];
+
+    const mockAction = {
+      type: 'COMICS_FETCH_DATA_SUCCESS',
+      comic: {
+        id: 1009718,
+        name: 'Wolverine',
+        description:
+          "Born with super-human senses and the power to heal from almost any wound, Wolverine was captured by a secret Canadian organization and given an unbreakable skeleton and claws. Treated like an animal, it took years for him to control himself. Now, he's a premiere member of both the X-Men and the Avengers.",
+      },
+    };
+
+    //execution
+    const result = comics(undefined, mockAction);
+
+    //expectation
+    expect(result).toEqual(expected);
+  });
+});
