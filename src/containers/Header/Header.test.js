41aee552e2401fd053bdceeec34e81920b69084a
marcusp619 <marcusp619@gmail.com>
2018-11-07 00:48:58 -0700

    Add test in header to check update state onclick

--- src/containers/Header/Header.test.js
+++ src/containers/Header/Header.test.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import { shallow } from "enzyme";
 
-import { Header } from "./index";
+import { Header, Button } from "./index";
 
 describe("Header", () => {
   it("Header should match snapshot", () => {
     const wrapper = shallow(<Header />);
     expect(wrapper).toMatchSnapshot();
   });
+
   it("should update the state when toggleSignInForm is called", () => {
     const wrapper = shallow(<Header />);
     const expectedState = { isVisible: true };
@@ -17,4 +18,31 @@ describe("Header", () => {
 
     expect(wrapper.state()).toEqual(expectedState);
   });
+
+  it("should setState to true when button is clicked", () => {
+    const wrapper = shallow(<Header />);
+
+    wrapper
+      .find(Button)
+      .at(0)
+      .simulate("click");
+
+    expect(wrapper.state()).toEqual({ isVisible: true });
+  });
+
+  describe("mapStateToProps", () => {
+    it("should return an object with an user object", () => {
+      //setup
+      const mockState = {
+        comics: [
+          {
+            id: 1009718,
+            name: "Wolverine",
+            description:
+              "Born with super-human senses and the power to heal from almost any wound, Wolverine was captured by a secret Canadian organization and given an unbreakable skeleton and claws. Treated like an animal, it took years for him to control himself. Now, he's a premiere member of both the X-Men and the Avengers."
+          }
+        ],
+        isLoading: undefined,
+        hasErrored: undefined
+      };
 });
--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -50,7 +50,7 @@ export const mapStateToProps = state => ({
 
 export default connect(mapStateToProps)(Header);
 
-const Button = styled.button`
+export const Button = styled.button`
   background: transparent;
   border-radius: 3px;
   border: 2px solid #AF7A6D;
