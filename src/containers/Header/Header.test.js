c5692ec8e4f1781d935926c6b3a55a3daf97fd5a
marcusp619 <marcusp619@gmail.com>
2018-11-07 01:43:40 -0700

    Update fetchcomic test

--- src/containers/Header/Header.test.js
+++ src/containers/Header/Header.test.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { shallow } from "enzyme";
 
-import { Header, Button } from "./index";
+import { Header, Button, mapStateToProps } from "./index";
 
 describe("Header", () => {
   it("Header should match snapshot", () => {
@@ -34,15 +34,18 @@ describe("Header", () => {
     it("should return an object with an user object", () => {
       //setup
       const mockState = {
-        comics: [
-          {
-            id: 1009718,
-            name: "Wolverine",
-            description:
-              "Born with super-human senses and the power to heal from almost any wound, Wolverine was captured by a secret Canadian organization and given an unbreakable skeleton and claws. Treated like an animal, it took years for him to control himself. Now, he's a premiere member of both the X-Men and the Avengers."
-          }
-        ],
-        isLoading: undefined,
-        hasErrored: undefined
+        user: { name: "mark", comics: [] }
       };
+
+      const expected = {
+        user: { name: "mark", comics: [] }
+      };
+
+      //execution
+      const mappedProps = mapStateToProps(mockState);
+
+      //expectation
+      expect(mappedProps).toEqual(expected);
+    });
+  });
 });
--- src/thunks/__tests__/fetchComics.test.js
+++ src/thunks/__tests__/fetchComics.test.js
@@ -1,6 +1,7 @@
 import { fetchComics } from "../fetchComic";
 import { isLoading, hasErrored, comicFetchDataSuccess } from "../../actions";
 import { dispatch } from "rxjs/internal/observable/pairs";
+import { cleanComicData } from "../../utils/DataCleaners";
 
 describe("fetchComic", () => {
   let mockDispatch;
@@ -34,15 +35,76 @@ describe("fetchComic", () => {
   it("should dispatch comicFetchDataSuccess when response is ok", async () => {
     window.fetch = jest.fn(() =>
       Promise.resolve({
-        ok: true
+        ok: true,
+        json: Promise.resolve(mockData)
       })
     );
+    const mockData = {
+      data: {
+        results: [
+          {
+            characters: { items: ["a", "b", "c"] },
+            description: "",
+            images: [
+              {
+                path:
+                  "http://i.annihil.us/u/prod/marvel/i/mg/b/b0/5aea30d90ef12",
+                extension: "jpg"
+              }
+            ],
+            prices: [{ type: "printPrice", price: 3.99 }],
+            thumbnail: "",
+            title: "",
+            urls: ["", ""],
+            offset: 0
+          },
+          {
+            characters: { items: ["a", "b", "c"] },
+            description: "",
+            images: [
+              {
+                path:
+                  "http://i.annihil.us/u/prod/marvel/i/mg/b/b0/5aea30d90ef12",
+                extension: "jpg"
+              }
+            ],
+            prices: [{ type: "printPrice", price: 3.99 }],
+            thumbnail: "",
+            title: "",
+            urls: ["", ""],
+            offset: 0
+          }
+        ]
+      }
+    };
+    const expected = [
+      {
+        characters: [undefined, undefined, undefined],
+        description: "",
+        images: "http://i.annihil.us/u/prod/marvel/i/mg/b/b0/5aea30d90ef12.jpg",
+        prices: "3.99",
+        thumbnail: "undefined.undefined",
+        title: "",
+        urls: ["", ""]
+      },
+      {
+        characters: [undefined, undefined, undefined],
+        description: "",
+        images: "http://i.annihil.us/u/prod/marvel/i/mg/b/b0/5aea30d90ef12.jpg",
+        prices: "3.99",
+        thumbnail: "undefined.undefined",
+        title: "",
+        urls: ["", ""]
+      }
+    ];
 
     const thunk = fetchComics();
 
     await thunk(mockDispatch);
 
     expect(mockDispatch).toHaveBeenCalledWith(isLoading(true));
+    await expect(cleanComicData(mockData)).toEqual(expected);
+    await cleanComicData(mockData);
     expect(mockDispatch).toHaveBeenCalledWith(isLoading(false));
   });
 });
