7528dd872a9c294c1d1b893b79e1de7fa69179ec
Mark Pearyer <marcusp619@gmail.com>
2018-11-08 10:13:28 -0700

    Update fetchComic.js

--- src/thunks/fetchComic.js
+++ src/thunks/fetchComic.js
@@ -2,9 +2,6 @@ import md5 from "crypto-js/md5";
 import { cleanComicData } from "../utils/DataCleaners";
 import { isLoading, hasErrored, comicFetchDataSuccess } from "../actions";
 
-const PRIV_KEY = "05198a9099469d502c9138bd02417830becd2e8f";
-const PUBLIC_KEY = "95d642f0dfe4844b8d9078f4e359a1af";
-
 export const fetchComics = () => {
   return async dispatch => {
     try {
